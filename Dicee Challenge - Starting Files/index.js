ed7bf5652fe38be021901863a25243b2d45b2aad
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-06-29 02:16:24 +0530

    Add files via upload

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,6 +1,36 @@
-var randonum1 = 4
+var randomum1 = Math.floor(Math.random()*6) + 1
 
-document.querySelector(".img1").setAttribute("src", "images/dice4.png") 
+var randomum2 = Math.floor(Math.random()*6) + 1
 
-document.querySelector(".img2").setAttribute("src", "./images/dice2.png") 
+// document.querySelector(".img1").setAttribute("src", "images/dice4.png") 
 
+// document.querySelector(".img2").setAttribute("src", "./images/dice2.png") 
+
+
+var randomimg = "dice"+randomum1+".png";
+
+var imgsrc = "images/"+randomimg
+
+var randomimg2 = "dice"+randomum2+".png";
+
+var imgsrc2 = "images/"+randomimg2
+
+
+
+var d1 = document.querySelectorAll("img")[0].setAttribute("src", imgsrc) 
+
+var d2 = document.querySelectorAll("img")[1].setAttribute("src", imgsrc2) 
+
+if(randomum1> randomum2){
+    document.querySelector("h1").innerText = "Plyaer1 wins"
+}
+
+else if(randomum1== randomum2){
+    document.querySelector("h1").innerText = "Draw"
+
+}
+
+else{
+    document.querySelector("h1").innerText = "Plyaer2 wins"
+
+}
